e4ab54c1ade1067d875a5e795230b062548c7253
Tronikel <contactdonatas@gmail.com>
2025-11-13 21:22:20 +0200

    feat: init

--- lua/blink-ctags/init.lua
+++ lua/blink-ctags/init.lua
@@ -1,27 +1,52 @@
---- @class blink.cmp.Source
-local Source = {}
+--- @class blink-ctags.Source.Opts
+--- @field max_items integer?
+--- @field kind_map table<string, integer>?
+--- @field prefix_search boolean?
+--- @field match_filename boolean?
+--- @field tagcase string?
+--- @field incomplete_threshold integer?
 
-function Source.new()
-	return setmetatable({}, { __index = Source })
+--- @class blink-ctags.Source
+--- @field opts blink-ctags.Source.Opts
+local Source = {
+	opts = {
+		max_items = nil,
+		kind_map = {
+			-- C,constant
+			-- a,augroup
+			-- c,command
+			-- f,function
+			-- k,class
+			-- m,map
+			-- n,filename
+			-- v,variable
+			C = vim.lsp.protocol.CompletionItemKind.Constant,
+			f = vim.lsp.protocol.CompletionItemKind.Function,
+			k = vim.lsp.protocol.CompletionItemKind.Class,
+			m = vim.lsp.protocol.CompletionItemKind.Method,
+			n = vim.lsp.protocol.CompletionItemKind.File,
+			v = vim.lsp.protocol.CompletionItemKind.Variable,
+		},
+		prefix_search = true,
+		match_filename = true,
+		tagcase = "ignore",
+		incomplete_threshold = 3,
+	},
+}
+
+function Source.new(opts)
+	opts = opts or {}
+	opts = vim.tbl_deep_extend("force", Source.opts, opts)
+	return setmetatable({ opts = opts }, { __index = Source })
 end
 
--- C,constant
--- a,augroup
--- c,command
--- f,function
--- k,class
--- m,map
--- n,filename
--- v,variable
-
-local kind_map = {
-	C = vim.lsp.protocol.CompletionItemKind.Constant,
-	f = vim.lsp.protocol.CompletionItemKind.Function,
-	k = vim.lsp.protocol.CompletionItemKind.Class,
-	m = vim.lsp.protocol.CompletionItemKind.Method,
-	n = vim.lsp.protocol.CompletionItemKind.File,
-	v = vim.lsp.protocol.CompletionItemKind.Variable,
-}
+---@return string
+function Source:get_prefix_search()
+	if self.opts.prefix_search then
+		return "^"
+	end
+	return ""
+end
 
 function Source:get_completions(ctx, callback)
 	local filename_origin = vim.fn.expand("%:p")
@@ -29,9 +54,10 @@ function Source:get_completions(ctx, callback)
 	local cmd = {
 		"nvim",
 		"--headless",
-		"+set tagcase=match",
+		string.format("+set tagcase=%s", self.opts.tagcase),
 		string.format(
-			[[+lua print(vim.json.encode(vim.fn.taglist("^%s", "%s")))]],
+			[[+lua print(vim.json.encode(vim.fn.taglist("%s%s", "%s")))]],
+			self:get_prefix_search(),
 			vim.fn.escape(ctx:get_keyword(), '"'),
 			vim.fn.escape(filename_origin, '"')
 		),
@@ -48,27 +74,40 @@ function Source:get_completions(ctx, callback)
 
 			local tags = vim.json.decode(out.stderr or "[]")
 
-			--- @type lsp.CompletionItem[]
+			---@type lsp.CompletionItem[]
 			local items = {}
 
 			for _, v in ipairs(tags) do
-				if #items == 100 then
+				if self.opts.max_items and #items == self.opts.max_items then
 					break
 				end
 
-				if vim.fn.fnamemodify(v.filename, ":e") == vim.fn.fnamemodify(filename_origin, ":e") then
-					table.insert(items, {
-						label = v.name,
-						detail = vim.trim(v.cmd:sub(3, -3)),
-						kind = kind_map[v.kind],
-						labelDetails = {
-							description = vim.fn.fnamemodify(v.filename, ":t"),
-						},
-					})
+				---@type lsp.CompletionItem
+				local item = {
+					label = v.name,
+					detail = vim.trim(v.cmd:sub(3, -3)),
+					kind = self.opts.kind_map[v.kind],
+					labelDetails = {
+						description = vim.fn.fnamemodify(v.filename, ":t"),
+					},
+				}
+
+				if self.opts.match_filename then
+					if vim.fn.fnamemodify(v.filename, ":e") == vim.fn.fnamemodify(filename_origin, ":e") then
+						table.insert(items, item)
+					end
+				else
+					table.insert(items, item)
 				end
 			end
 
-			callback({ items = items, is_incomplete_backward = true, is_incomplete_forward = false })
+			local incomplete = #ctx:get_keyword() < self.opts.incomplete_threshold
+
+			callback({
+				items = items,
+				is_incomplete_backward = incomplete,
+				is_incomplete_forward = incomplete,
+			})
 		end)
 	)
 
